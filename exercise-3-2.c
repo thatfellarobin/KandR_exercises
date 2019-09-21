966a532caa17194dbc78c9c0bbd350b945690789
Robin Liu <Robin.Liu.831@gmail.com>
2019-09-21 18:10:57 -0400

    second half of exercise 3-2, plus a bug fix

--- exercise-3-2.c
+++ exercise-3-2.c
@@ -9,18 +9,28 @@
 #define MAXLEN 1000
 
 int escape(char s[], char t[]);
+int unescape(char tt[], char ss[]);
 
 int main()
 {
-    int i, len;
+    int i, len, len2;
     char c, s[MAXLEN], t[MAXLEN];
+    char tt[MAXLEN], ss[MAXLEN];
 
+//    printf("enter test string for escape()\n");
+//    for (i = 0; (c = getchar()) != EOF && i < MAXLEN; ++i)
+//        s[i] = c;
+//    s[i] = '\0';
+//    len = escape(s, t);
+//    printf("%s\n", t);
+
+    printf("enter test string for unescape()\n");
     for (i = 0; (c = getchar()) != EOF && i < MAXLEN; ++i)
-        s[i] = c;
-
-    len = escape(s, t);
-
-    printf("%s\n", t);
+        tt[i] = c;
+    tt[i] = '\0';
+    len2 = unescape(tt, ss);
+    printf("%s\n", ss);
+    
     return 0;
 }
 
@@ -50,3 +60,39 @@ int escape(char s[], char t[])
     return j;
 }
 
+int unescape(char tt[], char ss[])
+{
+    int i, j = 0;
+    char c1, c2;
+
+    for (i = 0; (c1 = tt[i + j]) != '\0' && i + j + 1 < MAXLEN; ++i) {
+        switch (c1) {
+        case '\\':
+            ++j;
+            c2 = tt[i + j];
+            switch (c2) {
+            case 't':
+                ss[i] = '\t';
+                break;
+            case 'n':
+                ss[i] = '\n';
+                break;
+            case '\\': case '\0':
+                ss[i] = '\\';
+                j--;
+                break;
+            default:
+                ss[i] = c1;
+                j--;
+                break;
+            }
+            break;
+        default:
+            ss[i] = c1;
+            break;
+        }
+    }
+
+    ss[i] = '\0';
+    return i + j;
+}
